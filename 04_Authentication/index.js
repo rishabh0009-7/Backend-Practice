9985e2645e6e517f9a4d07d669298826a56300b4
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-06-16 08:25:39 +0530

    Basic auth  practice

---
+++ 04_Authentication/index.js
@@ -0,0 +1,136 @@
+// Basic way of Authentication in Node js app
+
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const user = [];
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  user.push({
+    username,
+    password,
+  });
+
+  res.json({
+    message: "you have signed up",
+  });
+});
+
+function generateToken() {
+  const options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let token = "";
+
+  for (let i = 0; i < 32; i++) {
+    token = token + options[Math.floor(Math.random() * options.length)];
+  }
+
+  return token;
+}
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = user.find(
+    (user) => user.username == username && user.password == password
+  );
+  if (user) {
+    const token = generateToken();
+    user.token = token;
+
+    res.send({
+      token,
+    });
+    console.log(user);
+  } else {
+    res.status(403).send({
+      message: "invalid username or password",
+    });
+  }
+});
+
+app.get("/me", (req, res) => {
+  const token = req.header.authorization;
+
+  const user = user.find((user) => user.token === token);
+  if (user) {
+    res.send({
+      username: user.username,
+    });
+  } else {
+    res.status(401).send({
+      message: "unauthorized",
+    });
+  }
+});
+
+app.listen(3000);
