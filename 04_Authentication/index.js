2b24d93d9dfa00b5ad73e40ed776f5464217bf6d
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-06-16 16:52:24 +0530

    jwt based auth

--- 04_Authentication/index.js
+++ 04_Authentication/index.js
@@ -1,13 +1,11 @@
 // Basic way of Authentication in Node js app
 
 import express from "express";
-import jwt from "jsonwebtoken"
 const app = express();
 
 app.use(express.json());
 
 const users = [];
-const JWT_SECRET= "USER_APP"
 
 app.post("/signup", (req, res) => {
   const username = req.body.username;
@@ -23,25 +21,96 @@ app.post("/signup", (req, res) => {
   });
 });
 
+function generateToken() {
+  const options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
 
+  let token = "";
+
+  for (let i = 0; i < 32; i++) {
+    token = token + options[Math.floor(Math.random() * options.length)];
+  }
+
+  return token;
+}
 
 app.post("/signin", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const existingUser = users.find(
+  const user = users.find(
     (user) => user.username == username && user.password == password
   );
-  if (existingUser) {
-    const token = jwt.sign({
-       username : existingUser.username 
-    },JWT_SECRET);
-    existingUser.token = token;
+  if (user) {
+    const token = generateToken();
+    user.token = token;
 
     res.send({
       token,
     });
-    console.log(existingUser);
+    console.log(users);
   } else {
     res.status(403).send({
       message: "invalid username or password",
@@ -49,65 +118,19 @@ app.post("/signin", (req, res) => {
   }
 });
 
-// app.get("/me", (req, res) => {
-//   const token = req.header.authorization;
-
-//   const userDetail = jwt.verify(token, JWT_SECRET)
-//   const username = userDetail.username
-
-//   const user = user.find((user) => user.username == username);
-//   if (user) {
-//     res.send({
-//       username: user.username,
-//     });
-//   } else {
-//     res.status(401).send({
-//       message: "unauthorized",
-//     });
-//   }
-// });
 
 
-
-// we will verify token now in auth middleware  not in /me endpoint
-
-function auth(req,res,next){
-  const token = req.headers.authorization
-
-  if(token){
-    jwt.verify(token , JWT_SECRET, (err, decoded)=>{
-      if(err){
-        res.status(401).send({
-          message :"unauthorized"
-        })
-      }else{
-        req.user = decoded;
-        next()
-      }
-
-    })
-  }else{
-    res.status(401).send({
-      message:"unauthorized "
-    })
-  }
-}
-
-
-
-app.get("/me", auth, (req, res) => {
-  const loggedinUser = users.find(
-    (user) => user.username === req.user.username
-  );
-  if (loggedinUser) {
+app.get("/me", (req, res) => {
+  const token = req.headers.authorization;
+  const user = users.find((user) => user.token === token);
+  if (user) {
     res.send({
-      username: loggedinUser.username,
+      username: user.username,
     });
   } else {
     res.status(401).send({
-      message: "unauthorized",
+      message: "Unauthorized",
     });
   }
 });
-
 app.listen(3000);
--- 05_JwtAuth/Jwt.js
+++ 05_JwtAuth/Jwt.js
@@ -1,15 +1,19 @@
+// jwt based authentication
+
 import  express from "express"
+import jwt from "jsonwebtoken"
 const app = express()
 
 app.use(express.json())
 
-const user = [];
+const users = [];
+const JWT_SECRET = "USER_APP"
 
 app.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  user.push({
+  users.push({
     username,
     password,
   });
@@ -19,25 +23,25 @@ app.post("/signup", (req, res) => {
   });
 });
 
-const JWT_SECRET = "USER_APP"
+
 app.post("/signin", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const user = user.find(
+  const existingUser = users.find(
     (user) => user.username == username && user.password == password
   );
-  if (user) {
+  if (existingUser) {
     const token = jwt.sign({
-        username:user.username
+        username:existingUser.username
 
     },JWT_SECRET);
-    user.token = token 
+    existingUser.token = token 
 
     res.send({
       token,
     });
-    console.log(user);
+    console.log(existingUser);
   } else {
     res.status(403).send({
       message: "invalid username or password",
@@ -45,22 +49,43 @@ app.post("/signin", (req, res) => {
   }
 });
 
-app.get("/me", (req, res) => {
-  const token = req.header.authorization;
+function auth (req,res,next){
+  const token = req.headers.authorization 
+  if(token){
+    jwt.verify(token , JWT_SECRET, (err,decoded)=>{
+      if(err){
+        res.status(401).send({
+          message:"unauthorized "
+        })
+      }else{
+        req.user= decoded
+        next()
 
-  
+      }
 
-  const user = user.find((user) => user.token === token);
-  if (user) {
-    res.send({
-      username: user.username,
-    });
-  } else {
+
+    })
+  }else{
     res.status(401).send({
-      message: "unauthorized",
+      message:"unauthorized"
+    })  }
+
+}
+
+app.get('/me', auth , (req,res)=>{
+  const loggedInUser = users.find(user=>user.username === req.user.username)
+  if(loggedInUser){
+    res.send({
+      username: loggedInUser.username,
     });
+   
+  }else{
+    res.status(401).send({
+      message :"unauthorized"
+
+    })
   }
-});
+})
 
 app.listen(3000);
   
